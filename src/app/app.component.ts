dd7075e387f67f4c44a00a6aa43f497c8f52c78c
agiklo <54153910+agiklo@users.noreply.github.com>
2021-04-02 11:37:56 +0200

    feat: create logic for add, remove, done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  newTask :string;
+  taskList: Array<string> = [];
+  taskDone: Array<string> = [];
+
+  add(){
+    this.taskList.push(this.newTask);
+    this.newTask = '';
+  }
+
+  remove(task: string){
+    this.taskList = this.taskList.filter(e => e!== task);
+  }
+
+  done(task: string){
+    this.taskDone.push(task);
+    this.remove(task);
+  }
 }
